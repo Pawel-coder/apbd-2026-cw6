8ec078896033cfe98db6f6a66981afaa886131a1
Pawel-coder <s33980@pjwstk.edu.pl>
2026-04-15 23:21:36 +0200

    Modified RoomsController: adding [HttpGet], [HttpGet("{id}")], [HttpGet("building/{buildingCode}")], [HttpPost], [HttpPut("{id}")], [HttpDelete("{id}")]. Modified ReservationsController: adding [HttpGet], [HttpGet("{id}")], [HttpPost], [HttpPut("{id}")], [HttpDelete("{id}")], IsTimeConflict helping method

--- Tutorial6/Controllers/ReservationsController.cs
+++ Tutorial6/Controllers/ReservationsController.cs
@@ -1,7 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using Tutorial6.Data;
+using Tutorial6.Models;
 namespace Tutorial6.Controllers;
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ReservationsController : ControllerBase
 {
-    
+        [HttpGet] //GET /api/reservations lub np. GET /api/reservations?date=2026-05-10&status=confirmed&roomId=2
+        public IActionResult Get([FromQuery] DateOnly? date, [FromQuery] string? status, [FromQuery] int? roomId)
+        {
+            var query = DataBase.Reservations.AsQueryable();
+            if (date.HasValue)
+                query = query.Where(r=>r.Date==date.Value);
+            if (!string.IsNullOrEmpty(status))
+                query = query.Where(r=>r.Status.Equals(status,StringComparison.OrdinalIgnoreCase));
+            if (roomId.HasValue)
+                query = query.Where(r=>r.RoomId==roomId.Value);
+            return Ok(query.ToList());
+        }
+        [HttpGet("{id}")] //GET /api/reservations/{id}
+        public IActionResult GetById(int id)
+        {
+            var reservation = DataBase.Reservations.FirstOrDefault(r=>r.Id==id);
+            if (reservation == null)
+                return NotFound($"Reservation o podanym ID {id} nie został znaleziony.");
+            return Ok(reservation);
+        }
+        [HttpPost] //POST /api/reservations
+        public IActionResult Add([FromBody] Reservation reservation)
+        {
+            var room = DataBase.Rooms.FirstOrDefault(r => r.Id == reservation.RoomId);
+            if (room == null)
+                return NotFound($"Reservation o podanym ID {reservation.RoomId} nie został znaleziony.");
+            if (!room.IsActive)
+                return BadRequest("Nie wolno dodać Reservation dla Room oznaczonego jako nieaktywny");
+            if (IsTimeConflict(reservation.RoomId, reservation.Date, reservation.StartTime, reservation.EndTime))
+                return Conflict("Dwie Reservation tego samego Room nie mogą nakładać się czasowo tego samego dnia.");
+            reservation.Id = DataBase.Reservations.Any()?DataBase.Reservations.Max(r=>r.Id)+1:1;
+            DataBase.Reservations.Add(reservation);
+            return CreatedAtAction(nameof(GetById), new {id=reservation.Id},reservation);
+        }
+        [HttpPut("{id}")] //PUT /api/reservations/{id}
+        public IActionResult Update(int id, [FromBody] Reservation updatedReservation)
+        {
+            var reservation = DataBase.Reservations.FirstOrDefault(r=>r.Id==id);
+            if (reservation == null)
+                return NotFound($"Reservation o podanym ID {id} nie został znaleziony.");
+            var room = DataBase.Rooms.FirstOrDefault(r => r.Id == updatedReservation.RoomId);
+            if (room == null) return NotFound($"Room o podanym ID {updatedReservation.RoomId} nie został znaleziony.");
+            if (!room.IsActive) return BadRequest($"Room o podanym ID {updatedReservation.RoomId} nie jest aktywny.");
+            if (IsTimeConflict(updatedReservation.RoomId, updatedReservation.Date, updatedReservation.StartTime, updatedReservation.EndTime, id))
+                return Conflict("Dwie Reservation tego samego Room nie mogą nakładać się czasowo tego samego dnia.");
+            reservation.RoomId = updatedReservation.RoomId;
+            reservation.OrganizerName = updatedReservation.OrganizerName;
+            reservation.Topic = updatedReservation.Topic;
+            reservation.Date = updatedReservation.Date;
+            reservation.StartTime = updatedReservation.StartTime;
+            reservation.EndTime = updatedReservation.EndTime;
+            reservation.Status = updatedReservation.Status;
+            return Ok(reservation);
+        }
+        [HttpDelete("{id}")] //DELETE /api/reservations/{id}
+        public IActionResult Delete(int id)
+        {
+            var reservation = DataBase.Reservations.FirstOrDefault(r=>r.Id==id);
+            if (reservation == null)
+                return NotFound($"Reservation o podanym ID {id} nie został znaleziony.");
+            DataBase.Reservations.Remove(reservation);
+            return NoContent();
+        } 
+        private bool IsTimeConflict(int roomId, DateOnly date, TimeOnly startTime, TimeOnly endTime, int? excceptionId = null)
+        {
+            return DataBase.Reservations.Any(r=>
+                r.RoomId == roomId &&
+                r.Date == date &&
+                startTime < r.EndTime && endTime > r.StartTime &&
+                r.Status != "cancelled" &&
+                r.Id != excceptionId
+            );
+        }
 }
--- Tutorial6/Controllers/RoomsController.cs
+++ Tutorial6/Controllers/RoomsController.cs
@@ -1,7 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Tutorial6.Data;
+using Tutorial6.Models;
 namespace Tutorial6.Controllers;
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class RoomsController : ControllerBase
 {
-    
+        [HttpGet] //GET /api/rooms lub np. GET /api/rooms?minCapacity=20&hasProjector=true&activeOnly=true
+        public IActionResult Get([FromQuery]int?minCapacity,[FromQuery]bool?hasProjector,[FromQuery]bool?activeOnly)
+        {
+            var query = DataBase.Rooms.AsQueryable();
+            if (minCapacity.HasValue)
+                query = query.Where(r=>r.Capacity>=minCapacity.Value);
+            if (hasProjector.HasValue)
+                query = query.Where(r=>r.HasProjector==hasProjector.Value);
+            if (activeOnly.HasValue&&activeOnly.Value)
+                query = query.Where(r=>r.IsActive);
+            return Ok(query.ToList());
+        }
+        [HttpGet("{id}")] //GET /api/rooms/{id}
+        public IActionResult GetById(int id)
+        {
+            var room = DataBase.Rooms.FirstOrDefault(r=>r.Id==id);
+            if (room==null)
+                return NotFound($"Room o podanym ID {id} nie został znaleziony.");
+            return Ok(room);
+        }
+        [HttpGet("building/{buildingCode}")] //GET /api/rooms/building/{buildingCode}
+        public IActionResult GetByBuilding(string buildingCode)
+        {
+            var rooms = DataBase.Rooms
+                .Where(r =>r.BuildingCode.Equals(buildingCode,StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            return Ok(rooms);
+        }
+        [HttpPost] //POST /api/rooms
+        public IActionResult Add([FromBody] Room room)
+        {
+            room.Id = DataBase.Rooms.Any()?DataBase.Rooms.Max(r=>r.Id)+1:1;
+            DataBase.Rooms.Add(room);
+            return CreatedAtAction(nameof(GetById), new {id=room.Id},room);
+        }
+        [HttpPut("{id}")] //POST /api/rooms
+        public IActionResult Update(int id, [FromBody] Room updatedRoom)
+        {
+            var room = DataBase.Rooms.FirstOrDefault(r=>r.Id==id);
+            if (room == null)
+                return NotFound($"Room o podanym ID {id} nie został znaleziony.");
+            room.Name = updatedRoom.Name;
+            room.BuildingCode = updatedRoom.BuildingCode;
+            room.Floor = updatedRoom.Floor;
+            room.Capacity = updatedRoom.Capacity;
+            room.HasProjector = updatedRoom.HasProjector;
+            room.IsActive = updatedRoom.IsActive;
+            return Ok(room);
+        }
+        [HttpDelete("{id}")] //DELETE /api/rooms/{id}
+        public IActionResult Delete(int id)
+        {
+            var room = DataBase.Rooms.FirstOrDefault(r=>r.Id==id);
+            if (room == null)
+                return NotFound($"Room o podanym ID {id} nie został znaleziony.");
+            var dateToday=DateOnly.FromDateTime(DateTime.Now);
+            var futureReservationsExist = DataBase.Reservations.Any(res=>res.RoomId==id&&res.Date>=dateToday);
+            if (futureReservationsExist)
+                return Conflict($"Nie można usunąć room o ID {id}, ponieważ istnieją dla niego przyszłe Reservations.");
+            DataBase.Rooms.Remove(room);
+            return NoContent();
+        }
 }
